210c0875f32cb738b0fde6cedeccc6f460979d03
Micah-Healy <97567232+Micah-Healy@users.noreply.github.com>
2022-04-21 11:29:26 -0600

    added overall structure
    Kelvin's code

---
+++ Mech.py
@@ -0,0 +1,53 @@
+from autonomous import auto
+
+
+class Tele:
+    high = 1000
+    low = 0
+
+
+
+
+def main(self ):
+    # tele
+
+    if auto():
+        print("yes")
+
+
+def forwardbackward(self, pin):
+    # read pin duration
+    # interpolate value to range
+    # check/cap value in range
+    # return value
+    return 0
+
+
+def leftright(self):
+    # read pin duration
+    # interpolate value to range
+    # check/cap value in range
+    # return value
+    return 0
+
+
+def kill(self):
+    # read in kill switch value
+    # if the value(s) read in are in a certain range, shut down
+    return 0
+
+
+def autonomous(self):
+    # read in autonomous/tele button value(s)
+    # if the value(s) are in their respiective ranges switch to auto or tele based on the range it is in
+    return 0
+
+
+def motorcontrol(self):
+    return 0
+
+
+# if __name__ == "__main__":
+#
+#    main()
+#    if button 1 == high
